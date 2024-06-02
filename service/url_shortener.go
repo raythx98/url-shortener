74e4263a636debefb7a74b035f17264972660828
Ray Toh <raythx98@gmail.com>
2024-06-03 01:03:36 +0800

    Add DB Pooling, default db logging

--- service/url_shortener.go
+++ service/url_shortener.go
@@ -2,9 +2,13 @@ package service
 
 import (
 	"context"
+	"fmt"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 	"github.com/raythx98/url-shortener/sqlc/url_mappings"
 	"github.com/raythx98/url-shortener/tools/sql_tool"
+	"log"
+	"os"
 	"time"
 
 	"github.com/google/uuid"
@@ -18,12 +22,44 @@ type IUrlShortener interface {
 type UrlShortener struct {
 }
 
+type myQueryTracer struct {
+	log *log.Logger
+}
+
+func (tracer *myQueryTracer) TraceQueryStart(
+	ctx context.Context,
+	_ *pgx.Conn,
+	data pgx.TraceQueryStartData) context.Context {
+	tracer.log.Println("Executing command", "sql", data.SQL, "args", data.Args)
+
+	return ctx
+}
+
+func (tracer *myQueryTracer) TraceQueryEnd(ctx context.Context, conn *pgx.Conn, data pgx.TraceQueryEndData) {
+}
+
 func (s *UrlShortener) ShortenUrl(ctx context.Context, url string) (string, error) {
-	conn, err := pgx.Connect(ctx, "user=postgres password=password host=localhost port=5432 dbname=url_shortener sslmode=disable") // TODO: connect on startup
+	// TODO: connect on startup
+	config, err := pgxpool.ParseConfig("user=postgres password=password host=localhost port=5432 dbname=url_shortener sslmode=disable pool_max_conns=10")
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to parse config: %v\n", err)
+		os.Exit(1)
+	}
+
+	config.ConnConfig.Tracer = &myQueryTracer{
+		log: log.Default(),
+	}
+
+	conn, err := pgxpool.NewWithConfig(context.Background(), config)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
+		os.Exit(1)
+	}
+
 	if err != nil {
 		return "", err
 	}
-	defer conn.Close(ctx)
+	defer conn.Close()
 
 	queries := url_mappings.New(conn)
 
@@ -43,11 +79,11 @@ func (s *UrlShortener) ShortenUrl(ctx context.Context, url string) (string, erro
 }
 
 func (s *UrlShortener) GetUrlWithShortened(ctx context.Context, shortenedUrl string) (string, error) {
-	conn, err := pgx.Connect(ctx, "user=postgres password=password host=localhost port=5432 dbname=url_shortener sslmode=disable") // TODO: connect on startup
+	conn, err := pgxpool.New(ctx, "user=postgres password=password host=localhost port=5432 dbname=url_shortener sslmode=disable") // TODO: connect on startup
 	if err != nil {
 		return "", err
 	}
-	defer conn.Close(ctx)
+	defer conn.Close()
 
 	queries := url_mappings.New(conn)
 
